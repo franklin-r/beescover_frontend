1f98cc074a4d5221eeb2e434b3bffd0aa6c65464
franklin-r <alexis.rossi97@gmail.com>
2025-07-20 04:54:06 +0200

    INSURANCE_CARD: Add data reads

--- components/shared/InsuranceCard.tsx
+++ components/shared/InsuranceCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -7,28 +9,83 @@ import {
 } from "@/components/ui/card";
 import { Button } from "../ui/button";
 import { Progress } from "@/components/ui/progress";
+import { Skeleton } from "../ui/skeleton";
 
-import { poolIdToPoolName } from "@/constants";
 import Link from "next/link";
+
+import { useReadContracts } from "wagmi";
+
+import { poolIdToPoolInfo, PoolInfo } from "@/constants";
 
 const InsuranceCard = ({ poolId }: { poolId: number }) => {
 
-	const insuranceName = poolIdToPoolName.get(poolId);
+	const poolInfo = poolIdToPoolInfo.get(poolId) as PoolInfo;
+	const {
+		data,
+		isLoading,
+		isSuccess,
+		error,
+		refetch
+	} = useReadContracts({
+		contracts: [
+			{
+				address: poolInfo.address,
+				abi: poolInfo.abi,
+				functionName: "govTokenApr"
+			},
+			{
+				address: poolInfo.address,
+				abi: poolInfo.abi,
+				functionName: "getAavePoolAPR"
+			},
+			{
+				address: poolInfo.address,
+				abi: poolInfo.abi,
+				functionName: "totalLiquidity"
+			},
+			{
+				address: poolInfo.address,
+				abi: poolInfo.abi,
+				functionName: "totalLocked"
+			}
+		]
+	});
+	const govTokenApr = Number(data?.[0]?.result) / 100;
+	const aavePoolApr = ((Number(data?.[1]?.result) / 1e27) * 100).toFixed(2);
+	const totalLiquidity = Number(data?.[2]?.result) / (10**poolInfo.assetDecimals);
+	const totalLocked = Number(data?.[3]?.result) / (10**poolInfo.assetDecimals);
+	const progress = (totalLocked / totalLiquidity) * 100;
 
 	return (
 		<Card className="w-full max-w-sm shadow-md hover:shadow-xl transition-shadow duration-100">
       <CardHeader>
-        <CardTitle className="text-2xl">{insuranceName}</CardTitle>
+        <CardTitle className="text-2xl">{poolInfo.name}</CardTitle>
       </CardHeader>
       <CardContent>
 				<div className="mb-5 space-y-4">
 					<h1 className="text-xl font-bold mb-2">APR</h1>
-					<p>3.6 % + 5 % $BEE</p>
+					{isLoading && (
+						<Skeleton className="h-[20px] w-[150px] rounded-full" />
+					)}
+					{error && (
+						<div className="text-red-500">Looks like there is a mistake...</div>
+					)}
+					{isSuccess && (
+						<div>{aavePoolApr} % {poolInfo.assetSymbol} + {govTokenApr?.toString()} % $BEE</div>
+					)}
 				</div>
 				<div className=" mb-5 space-y-4">
 					<h1 className="text-xl font-bold mb-2">Coverage Liquidity</h1>
-					<p>50 000 / 120 000 USDC</p>
-					<Progress value={42} className="bg-gray-200 [&>div]:bg-yellow-300" />
+					{isLoading && (
+						<Skeleton className="h-[20px] w-[200px] rounded-full" />
+					)}
+					{error && (
+						<div className="text-red-500">Looks like there is a mistake...</div>
+					)}
+					{isSuccess && (
+						<div>{totalLocked?.toString()} / {totalLiquidity?.toString()} {poolInfo.assetSymbol}</div>
+					)}
+					<Progress value={progress} className="bg-gray-200 [&>div]:bg-yellow-300" />
 				</div>
       </CardContent>
       <CardFooter className="flex gap-2">
--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,49 @@
-export let poolIdToPoolName = new Map<number, string>();
-poolIdToPoolName.set(0, "USDC Depeg");
-poolIdToPoolName.set(1, "WBTC Depeg");
-poolIdToPoolName.set(2, "EURS Depeg");
-poolIdToPoolName.set(3, "USDT Depeg");
+import { usdcInsurancePoolAbi, usdcInsurancePoolAddress, usdcInsurancePoolAdmin } from "./usdcInsurancePool";
+
+export type PoolInfo = {
+	name: string;
+	address: `0x${string}`;
+	admin: string;
+	abi: any[];
+	assetAddress: string;
+	assetSymbol: string;
+}
+
+const usdcPool: PoolInfo = {
+	name: "USDC Depeg",
+	address: usdcInsurancePoolAddress,
+	admin: usdcInsurancePoolAdmin,
+	abi: usdcInsurancePoolAbi,
+	assetAddress: "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8",
+	assetSymbol: "USDC"
+}
+const wbtcPool: PoolInfo = {
+	name: "WBTC Depeg",
+	address: usdcInsurancePoolAddress,
+	admin: usdcInsurancePoolAdmin,
+	abi: usdcInsurancePoolAbi,
+	assetAddress: "0x29f2D40B0605204364af54EC677bD022dA425d03",
+	assetSymbol: "WBTC"
+}
+const eursPool: PoolInfo = {
+	name: "EURS Depeg",
+	address: usdcInsurancePoolAddress,
+	admin: usdcInsurancePoolAdmin,
+	abi: usdcInsurancePoolAbi,
+	assetAddress: "0x6d906e526a4e2Ca02097BA9d0caA3c382F52278E",
+	assetSymbol: "EURS"
+}
+const usdtPool: PoolInfo = {
+	name: "USDT Depeg",
+	address: usdcInsurancePoolAddress,
+	admin: usdcInsurancePoolAdmin,
+	abi: usdcInsurancePoolAbi,
+	assetAddress: "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0",
+	assetSymbol: "USDT"
+}
+
+export let poolIdToPoolInfo = new Map<number, PoolInfo>();
+poolIdToPoolInfo.set(0, usdcPool);
+poolIdToPoolInfo.set(1, wbtcPool);
+poolIdToPoolInfo.set(2, eursPool);
+poolIdToPoolInfo.set(3, usdtPool);
