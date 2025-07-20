154df6cbd6eb57228a1573aeb84f4d7c6908ba46
franklin-r <alexis.rossi97@gmail.com>
2025-07-20 14:14:05 +0200

    INSURER_POSITION: Add transactions

--- components/shared/InsurerDashboard.tsx
+++ components/shared/InsurerDashboard.tsx
@@ -1,13 +1,48 @@
+"use client";
+
+import { useAccount, useReadContracts } from "wagmi";
+
 import InsurerPositionCard from "./InsurerPositionCard";
 
+import { poolIdToPoolInfo } from "@/constants";
+import { Skeleton } from "../ui/skeleton";
+
 const InsurerDashboard = () => {
-	// Display variable amount of positions
+
+	const {address} = useAccount();
+	const poolInfos = Array.from(poolIdToPoolInfo.entries()); // [[0, info], [1, info], ...]
+
+  const {
+    data: lpBalances,
+    isLoading: lpBalancesIsLoading,
+		isSuccess: lpBalancesIsSuccess,
+    error: lpBalancesError
+  } = useReadContracts({
+    contracts: poolInfos.map(([poolId, info]) => ({
+      address: info.lpTokenAddress,
+      abi: info.lpTokenAbi,
+      functionName: "balanceOf",
+      args: [address],
+    }))
+  });
+
 	return (
 		<>
 			<div className="space-y-6 w-full">
 				<h1 className="text-3xl font-bold text-center">Insurer Dashboard</h1>
-				<InsurerPositionCard />
-				<InsurerPositionCard />
+				{lpBalancesIsLoading && (
+					<Skeleton className="h-[250px] w-[590px]" />
+				)}
+				{lpBalancesError && (
+					<div className="text-red-500">Looks like there is a mistake...</div>
+				)}
+				{lpBalancesIsSuccess && (
+					poolInfos.map(([poolId, info]) => {
+						const balance = lpBalances[poolId]?.result;
+						if (!balance || BigInt(Number(balance)) === 0n) return null;
+						return <InsurerPositionCard key={poolId} poolId={poolId} />
+					})
+				)}
 			</div>
 		</>
 	);
--- components/shared/InsurerPositionCard.tsx
+++ components/shared/InsurerPositionCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -9,27 +11,87 @@ import { Button } from "../ui/button";
 
 import Link from "next/link";
 
-const InsurerPositionCard = () => {
+import { poolIdToPoolInfo, PoolInfo } from "@/constants";
+
+import { useAccount, useReadContracts } from "wagmi";
+import { Skeleton } from "../ui/skeleton";
+
+const InsurerPositionCard = ({ poolId }: { poolId: number }) => {
+	const poolInfo = poolIdToPoolInfo.get(poolId) as PoolInfo;
+	const {address} = useAccount();
+
+	const {
+		data,
+		isLoading,
+		isSuccess,
+		error
+	} = useReadContracts({
+		contracts: [
+			{
+				address: poolInfo.address,
+				abi: poolInfo.abi,
+				functionName: "govTokenApr"
+			},
+			{
+				address: poolInfo.address,
+				abi: poolInfo.abi,
+				functionName: "getAavePoolAPR"
+			},
+			{
+				address: poolInfo.lpTokenAddress,
+				abi: poolInfo.lpTokenAbi,
+				functionName: "balanceOf",
+				args: [address!]
+			}
+		],
+		query: {
+			enabled: !!address
+		}
+	});
+	const govTokenApr = Number(data?.[0]?.result) / 100;
+	const aavePoolApr = ((Number(data?.[1]?.result) / 1e27) * 100).toFixed(2);
+	const lpBalance = Number(data?.[2]?.result) / (10**poolInfo.lpTokenDecimals);
+
 	return (
 		<Card className="w-full shadow-md hover:shadow-xl transition-shadow duration-100">
       <CardHeader>
-        <CardTitle className="text-2xl">USDC Depeg</CardTitle>
+        <CardTitle className="text-2xl">{poolInfo.name}</CardTitle>
       </CardHeader>
       <CardContent className="space-y-4">
 				<div className="flex justify-between">
 					<p className="text-lg">LP Tokens</p>
-					<p>12 345 LP Tokens</p>
+					{isLoading && (
+						<Skeleton className="h-[20px] w-[150px] rounded-full" />
+					)}
+					{error && (
+						<div className="text-red-500">Looks like there is a mistake...</div>
+					)}
+					{isSuccess && (
+						<p>{lpBalance} {poolInfo.lpTokenSymbol}</p>
+					)}
+
 				</div>
 				<div className="flex justify-between">
 					<p className="text-lg">APR</p>
-					<p>3.45 % + 5 % $BEE</p>
+					{isLoading && (
+						<Skeleton className="h-[20px] w-[150px] rounded-full" />
+					)}
+					{error && (
+						<div className="text-red-500">Looks like there is a mistake...</div>
+					)}
+					{isSuccess && (
+						<div>{aavePoolApr} % {poolInfo.assetSymbol} + {govTokenApr?.toString()} % $BEE</div>
+					)}
 				</div>
       </CardContent>
-      <CardFooter className="justify-center">
-				<Button className="bg-black">
-					<Link href="">View Pool</Link>
-				</Button>
-      </CardFooter>
+      <CardFooter className="flex gap-2">
+				<Button className="flex-1 bg-black">
+					<Link href={`/insurer?poolId=${poolId}`}>View Pool</Link>
+        </Button>
+        <Button className="flex-1 bg-black">
+          <Link href="">Withdraw</Link>
+        </Button>
+			</CardFooter>
     </Card>
 	);
 }
